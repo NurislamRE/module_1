426695eb1d6cb3a6fc7981c32e485bbf0e3d97cc
Нурислам Раушанбек <nurislam.raushanbek@gmail.com>
2022-03-28 15:31:48 +0600

    Добавил второй вид товара

--- lesson_02/script.js
+++ lesson_02/script.js
@@ -1,12 +1,20 @@
-
-
 'use strict';
+{
+    const productName = 'Hewlet-Packard';
+    const productCount = 5;
+    const productCategory = 'Мониторы';
+    const productPrice = 50000;
+    
+    console.log(productName);
+    console.log('Сумма: ' + productPrice*productCount);
+}
 
-const productName = 'Hewlet-Packard';
-const productCount = 5;
-const productCategory = "Мониторы";
-const productPrice = 50000;
+{
+    const productName = 'Samsung S10';
+    const productCount = 10;
+    const productCategory = 'Смартфоны';
+    const productPrice = 40000;
 
-console.log(productName);
-console.log(productPrice*productCount);
-
+    console.log(productName);
+    console.log('Сумма: ' + productPrice*productCount);
+}
