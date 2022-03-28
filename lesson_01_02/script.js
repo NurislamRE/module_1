0055328dd2ef46ff2fefabf8ba097976313eade0
Нурислам Раушанбек <nurislam.raushanbek@gmail.com>
2022-03-28 11:41:06 +0600

    initial

---
+++ lesson_01_02/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const productName = 'Hewlet-Packard';
+const productCount = 5;
+const productCategory = "Мониторы";
+const productPrice = 50000;
+
+console.log(productPrice*productCount);
+
